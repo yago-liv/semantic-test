e078fcd1628f3afdb56fb468371b905c8295d344
Yago Neno <developer.neno@gmail.com>
2025-03-16 00:54:21 -0300

    chore: test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutletd],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
