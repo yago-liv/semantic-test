fd079c9549e5d7efa6da14b1891ead52e297b81a
Yago Neno <developer.neno@gmail.com>
2025-03-16 00:58:09 -0300

    fix: imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutletd],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
